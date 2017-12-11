45e471699f611ea12eca7a945add5bb9de8609dd
Martin <kralmar3@fel.cvut.cz>
2017-12-11 17:51:51 +0100

    Minor update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-Matlab&Simulink interface to Ardupilot SITL.
+#Matlab&Simulink interface to ArduPilot SITL
+
+##How to run
+1. Run your ArduPilot SITL instance in Gazebo mode. You'll have to run script like "sim_vehicle.py --model gazebo"
+2. Optionally connect your GCS
+3. Run init.m from Matlab
+4. Run Simulink model with connected ArduPilot SITL interface blocks
 
 It uses Python scripts so you have to initialize Python in Matlab.
--- init.m
+++ init.m
@@ -1,9 +1,17 @@
-sim_out_address = py.tuple({'127.0.0.1', py.int(9003)});
+ip = '127.0.0.1';
+simOutPort = 9003;
+simInPort = 9002;
+sitl.output.motors = [5 6 7 8];
+sitl.output.servos = [1 2 3 4];
+sitl.location.latLon = [-35.362938, 149.165085];
+sitl.location.height = -585;
+
+sim_out_address = py.tuple({ip, py.int(simOutPort)});
 sim_out = py.socket.socket(py.socket.AF_INET, py.socket.SOCK_DGRAM);
 sim_out.connect(sim_out_address);
 sim_out.setblocking(0);
 
-sim_in_address = py.tuple({'127.0.0.1', py.int(9002)});
+sim_in_address = py.tuple({ip, py.int(simInPort)});
 sim_in = py.socket.socket(py.socket.AF_INET, py.socket.SOCK_DGRAM);
 sim_in.bind(sim_in_address);
 sim_in.setblocking(1);
